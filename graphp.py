72c084da17a4a7f18b8b36d3a84e8e6495817bec
Igor <yudin-igor@rambler.ru>
2021-05-22 19:27:35 +0300

    Добавил перемотку графика

--- graphp.py
+++ graphp.py
@@ -2,6 +2,7 @@ import pandas
 import mplfinance as mpf
 import matplotlib.pyplot as plt
 import datetime
+from time import sleep
 
 
 # default
@@ -9,48 +10,48 @@ import datetime
 # dt_default = dt_default.history(period="1mo")  # 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
 # print(dt_default)
 
+class Graph:
 
-def ts_to_datetime(ts, d=None):
-    result = datetime.datetime.fromtimestamp(ts / 1000.0)
-    if d is None:
-        return result.strftime('%Y-%m-%d %H:%M')
-    else:
+    @staticmethod
+    def ts_to_datetime(ts, d=None):
+        result = datetime.datetime.fromtimestamp(ts / 1000.0)
+        if d is None:
+            return result.strftime('%Y-%m-%d %H:%M')
+        else:
+            return result
+
+    def json_to_dt(self, lst_original, count=None, shift=0):
+        result = []
+        temp = lst_original.copy()
+        j = 0
+        for i in temp:
+            j += 1
+            temp_dic = {}
+            if count is None:
+                x = self.ts_to_datetime(ts=i['t'], d=True)
+                date = pandas.to_datetime(x)
+                y = {"Date": date, "Open": i["o"], "High": i["h"], "Low": i["l"], "Close": i["c"], "Volume": i["v"]}
+                temp_dic.update(y)
+                result.append(temp_dic)
+            else:
+                if (count + shift) >= j > shift:
+                    x = self.ts_to_datetime(ts=i['t'], d=True)
+                    date = pandas.to_datetime(x)
+                    y = {"Date": date, "Open": i["o"], "High": i["h"], "Low": i["l"], "Close": i["c"], "Volume": i["v"]}
+                    temp_dic.update(y)
+                    result.append(temp_dic)
+        print("Число свеч: {}".format(len(result)))
         return result
 
+    def save_graph(self, data, count, shift=0):
 
-def json_to_dt(lst_original, count=None, reverse=None):
-    result = []
-    temp = lst_original.copy()
-    j = 0
-    for i in temp:
-        j += 1
-        temp_dic = {}
-        if count is None:
-            x = ts_to_datetime(ts=i['t'], d=True)
-            date = pandas.to_datetime(x)
-            temp_dic.update(
-                {"Date": date, "Open": i["o"], "High": i["h"], "Low": i["l"], "Close": i["c"], "Volume": i["v"]})
-            result.append(temp_dic)
-        else:
-            if count >= j:
-                x = ts_to_datetime(ts=i['t'], d=True)
-                date = pandas.to_datetime(x)
-                temp_dic.update(
-                    {"Date": date, "Open": i["o"], "High": i["h"], "Low": i["l"], "Close": i["c"], "Volume": i["v"]})
-                result.append(temp_dic)
-    print("Число свеч: {}".format(len(result)))
-    return result
+        def save_to_png(dataframe, filename="graph.png"):
+            plt.figure(figsize=(26, 18))
+            z = mpf.plot(dataframe, type='candle', returnfig=True)
+            z.savefig('static/{}'.format(filename))
+            print("{} [save_graph_to_png] Изображение сохранено!".format(datetime.datetime.now()))
 
-
-def save_graph_to_png(dataframe):
-    plt.figure(figsize=(26, 18))
-    z = mpf.plot(dataframe, type='candle', returnfig=True)
-    z.savefig('static/graph.png')
-    print("Сохранено!")
-
-
-def save_graph(data):
-    lst = json_to_dt(data[1], count=14)
-    dt = pandas.DataFrame(lst)
-    dt = dt.set_index(['Date'])
-    save_graph_to_png(dataframe=dt)
+        lst = self.json_to_dt(data, count=count, shift=shift)
+        dt = pandas.DataFrame(lst)
+        dt = dt.set_index(['Date'])
+        save_to_png(dataframe=dt)
--- start.py
+++ start.py
@@ -2,13 +2,17 @@ from time import sleep
 from polygon import RESTClient
 from flask import Flask, render_template, request
 import datetime
-from graphp import save_graph
+from graphp import Graph
 from threading import Thread
 import multiprocessing
 
-ticker = "MSFT"
-period = 15
 
+# Вариант 1 (не работает)
+# save_graph(data=data, count=14)
+# Вариант 2 (не работает)
+# Thread(target=save_graph, args=(data, 14)).start()
+# Вариант 3 (работает)
+# multiprocessing.Process(None, save_graph, args=(data, 14)).start()
 
 def ts_to_datetime(ts, d=None):
     result = datetime.datetime.fromtimestamp(ts / 1000.0)
@@ -18,17 +22,16 @@ def ts_to_datetime(ts, d=None):
         return result
 
 
-def get_bars(ticker, period, date_from, date_to):
+def get_bars(t, p, date_from, date_to):
     key = "P4KJxw1ZvsZ9JDxWPGS2VXvBnxCpDi8H"
     with RESTClient(key) as client:
-        title = f'Данные для данные для "{ticker}" с {date_from} по {date_to} (Период: {period} минут)'
-        resp = client.stocks_equities_aggregates(ticker, period, "minute", date_from, date_to, unadjusted=False)
-        # sleep(1)
-        print("Получено свеч:", len(resp.results))
-        return [resp, resp.results, title]
+        title = f'Данные для данные для "{t}" с {date_from} по {date_to} (Период: {period} минут)'
+        resp = client.stocks_equities_aggregates(t, p, "minute", date_from, date_to, unadjusted=False)
+        print("{} [get_bars] Принято свеч: {}".format(datetime.datetime.now(), len(resp.results)))
+        return {"Main": resp, "Candles": resp.results, "Title": title}
 
 
-def print_table(resp):
+def get_table(resp):
     sleep(1)
     result_text = []
     for result in resp.results:
@@ -39,43 +42,86 @@ def print_table(resp):
     return result_text
 
 
+# Умная пауза
+def wait(thread, delay=5):
+    count = 0
+    for i in range(delay * 1000):
+        count += 1
+        sleep(0.001)
+        if not thread.is_alive():
+            print("{} [wait] Пауза закончилась. (Iterations={})".format(datetime.datetime.now(), count))
+            return True
+    print("{} [wait] Пауза закончилась. (Iterations=MAX)".format(datetime.datetime.now()))
+    return False
+
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
+    global position
     global ticker
     global period
+    global data
+    global table
+    global count
 
     if request.method == 'GET':  # Запрос на чтение (к примеру при обновлении страницы Flask)
-        data = get_bars(ticker=ticker, period=period, date_from="2021-04-18", date_to="2021-05-18")
-
-        # Вариант 1 (не работает)
-        # save_graph(data=data)
-        # Вариант 2 (не работает)
-        # Thread(target=save_graph, args=(data, )).start()
-        # Вариант 3 (работает)
-        multiprocessing.Process(None, save_graph, args=(data,)).start()
-
-        json = print_table(data[0])
-        return render_template('index.html', TITLE=data[2], JSON={"ticker": ticker, "period": period, "table": json})
+        th = multiprocessing.Process(None, graph.save_graph, args=(data["Candles"], position[0], position[1]))
+        th.start()
+        wait(th)
+        progress = [position[1] + count + 1, len(data["Candles"])]
+        json = {"ticker": ticker, "period": period, "table": table, "progress": progress}
+        return render_template('index.html', TITLE=data["Title"], JSON=json)
     if request.method == 'POST':
-        ticker = request.form['ticket']
-        period = request.form['period']
+        if request.form['submit'] == "Отправить":
 
-        data = get_bars(ticker=ticker, period=period, date_from="2021-04-18", date_to="2021-05-18")
+            count = 6
+            position = [count + 1, 0]  # count, shift
 
-        # Вариант 1 (не работает)
-        # save_graph(data=data)
-        # Вариант 2 (не работает)
-        # Thread(target=save_graph, args=(data, )).start()
-        # Вариант 3 (работает)
-        multiprocessing.Process(None, save_graph, args=(data,)).start()
+            ticker = request.form['ticket']
+            period = request.form['period']
+            data = get_bars(t=ticker, p=period, date_from="2021-04-18", date_to="2021-05-18")
+            th = multiprocessing.Process(None, graph.save_graph, args=(data["Candles"], position[0], position[1]))
+            th.start()
+            wait(th)
+            table = get_table(data["Main"])
+            progress = [position[1] + count + 1, len(data["Candles"])]
+            json = {"ticker": ticker, "period": period, "table": table, "progress": progress}
+            return render_template('index.html', TITLE=data["Title"], JSON=json)
+        elif request.form['submit'] == "Вперед":
+            print(len(data["Candles"]))
+            if position[1] + count + 1 < len(data["Candles"]):
+                position[1] += count
 
-        json = print_table(data[0])
-        return render_template('index.html', TITLE=data[2], JSON={"ticker": ticker, "period": period, "table": json})
+            progress = [position[1] + count + 1, len(data["Candles"])]
+            th = multiprocessing.Process(None, graph.save_graph, args=(data["Candles"], position[0], position[1]))
+            th.start()
+            wait(th)
+            json = {"ticker": ticker, "period": period, "table": table, "progress": progress}
+            return render_template('index.html', TITLE=data["Title"], JSON=json)
+        elif request.form['submit'] == "Назад":
+            if position[1] - count > 0:
+                position[1] -= count
+            progress = [position[1] + count + 1, len(data["Candles"])]
+            th = multiprocessing.Process(None, graph.save_graph, args=(data["Candles"], position[0], position[1]))
+            th.start()
+            wait(th)
+            json = {"ticker": ticker, "period": period, "table": table, "progress": progress}
+            return render_template('index.html', TITLE=data["Title"], JSON=json)
 
 
 if __name__ == '__main__':
+
+    graph = Graph()
+
+    ticker = "MSFT"
+    period = 15
+    data = get_bars(t=ticker, p=period, date_from="2021-04-18", date_to="2021-05-18")
+    table = get_table(data["Main"])
+
+    count = 6
+    position = [count + 1, 0]  # count, shift
+
     app.run()
